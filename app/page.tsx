66a53c4d52cb5dadbd443a870ac061bfe0027912
Jetnor Muhaj <jmuhaj@gmail.com>
2023-06-18 19:01:27 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,28 +19,27 @@ export default function HomePage() {
           <span className="text-blue-600">Vercel</span>
         </a>
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl">
-          Generating dream rooms{" "}
+          Generating creative QR code{" "}
           <span className="relative whitespace-nowrap text-blue-600">
             <SquigglyLines />
             <span className="relative">using AI</span>
           </span>{" "}
-          for everyone.
+          in seconds.
         </h1>
         <h2 className="mx-auto mt-12 max-w-xl text-lg sm:text-gray-400  text-gray-500 leading-7">
-          Take a picture of your room and see how your room looks in different
-          themes. 100% free – remodel your room today.
+          Take a traditional existing QR code and make it more eye-catching, transform it into a visually artistic AI art which might be really useful for brand communication. 100% free – redesign your QR codes today.
         </h2>
         <Link
           className="bg-blue-600 rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-blue-500 transition"
           href="/dream"
         >
-          Generate your dream room
+          Generate your QR code art
         </Link>
         <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
           <div className="flex flex-col space-y-10 mt-4 mb-16">
             <div className="flex sm:space-x-8 sm:flex-row flex-col">
               <div>
-                <h3 className="mb-1 font-medium text-lg">Original Room</h3>
+                <h3 className="mb-1 font-medium text-lg">Original QR code</h3>
                 <Image
                   alt="Original photo of a room with roomGPT.io"
                   src="/original-pic.jpg"
@@ -50,9 +49,9 @@ export default function HomePage() {
                 />
               </div>
               <div className="sm:mt-0 mt-8">
-                <h3 className="mb-1 font-medium text-lg">Generated Room</h3>
+                <h3 className="mb-1 font-medium text-lg">Generated QR code</h3>
                 <Image
-                  alt="Generated photo of a room with roomGPT.io"
+                  alt="Generated photo of a QR code with aiin.io"
                   width={400}
                   height={400}
                   src="/generated-pic-2.jpg"
