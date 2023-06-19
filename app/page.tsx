65d65578c0e80e5b6ca4b3a1e794e1e810093d70
Jetnor Muhaj <jmuhaj@gmail.com>
2023-06-19 11:02:10 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-20 mt-20 background-gradient">
         <a
-          href="https://vercel.fyi/roomGPT"
+          href="https://github.com/muhaj/aiqrcode"
           target="_blank"
           rel="noreferrer"
           className="border border-gray-700 rounded-lg py-2 px-4 text-gray-400 text-sm mb-5 transition duration-300 ease-in-out"
